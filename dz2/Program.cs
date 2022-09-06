70d18df24389bf68b95763d1efbe0e15cfc71b20
Kashira2 <nekit.egorov@icloud.com>
2022-09-06 23:25:22 +0300

    dz2

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] Massive()
+{   
+    Console.WriteLine("Укажите размер массива: ");
+    int N = int.Parse(Console.ReadLine());
+    int[] array = new int[N];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i] = new Random().Next(0, 999)}, ");
+        else Console.Write($"{array[i] = new Random().Next(0, 999)}.");
+    }
+    return array;
+}
+
+string CountNumber(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i=i+2)
+    {
+        sum = array[i] + sum;
+    }
+    return $"{sum}";
+}
+
+Console.WriteLine();
+Console.WriteLine($" Сумма чисел на нечетных позициях в массиве = {CountNumber(Massive())}.");
