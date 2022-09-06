4ae4ea5997b40f3c4dc8912710787a0e0e91aefa
Kashira2 <nekit.egorov@icloud.com>
2022-09-07 00:03:15 +0300

    dz3

--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] Massive()
+{   
+    Console.WriteLine("Укажите размер массива: ");
+    int N = int.Parse(Console.ReadLine());
+    int[] array = new int[N];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i != array.Length - 1) Console.Write($"{array[i] = new Random().Next(0, 100)}, ");
+        else Console.Write($"{array[i] = new Random().Next(0, 100)} ");
+    }
+    return array;
+}
+
+int[] array = Massive();
+
+Console.WriteLine();
+
+string ArrayMaxMin(int[] array)
+{   
+    int max = 0;
+    int min = 100;
+    double sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max < array[i]) 
+        {
+            max = array[i];
+        }
+        if (min > array[i])
+        { 
+            min = array[i];
+        }
+        sum += array[i];
+    }
+    
+    return $"Разница между максимальным и минимальным числом в массиве = {max - min}";
+}
+
+
+Console.WriteLine(ArrayMaxMin(array));
